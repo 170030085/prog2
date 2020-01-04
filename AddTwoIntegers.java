5c74f18a2c965d9a8ad5b322503364e3831a42eb
170030085 <54091697+170030085@users.noreply.github.com>
2020-01-04 15:13:00 +0530

    Add files via upload

---
+++ AddTwoIntegers.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.util.Scanner;
+class AddTwoIntegers {
+    public static void main(String[] args) {
+        
+        int a= 10;
+        int b = 20;
+        int sum = a+b;
+        System.out.println("The sum is: " + sum);
+    }
+}
